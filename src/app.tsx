595df4a1953d8530438752651e347c114e7dda48
ColorFilter <johan@romantech.net>
2024-02-13 21:16:53 +0900

    Github 아이콘 버튼 추가

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,14 @@
+import { GithubButton } from '@/components';
 import { GameOptionProvider, ScreenProvider } from '@/context';
 import { Screen } from '@/pages';
 
 function App() {
   return (
     <main className="min-h-screen bg-slate-800">
+      <GithubButton
+        url="https://github.com/romantech/tic-tac-toe"
+        className="absolute right-4 top-4"
+      />
       <ScreenProvider>
         <GameOptionProvider>
           <Screen />
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,4 +3,5 @@ export { default as Empty } from './empty';
 export { default as Button } from './button';
 export { default as Divider } from './divider';
 export { default as Fade } from './fade';
+export { default as GithubButton } from './github-button';
 export * from './box';
