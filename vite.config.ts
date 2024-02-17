c7fe17fdd9e0b33f7a94bdd2acc88459c19e579e
ColorFilter <johan@romantech.net>
2024-02-17 16:16:31 +0900

    manifest.json 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -7,6 +8,13 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [
     react(),
+    /**
+     * vite-plugin-pwa 플러그인으로 서비스 워커 스크립트 자동 등록
+     * 서비스 워커는 웹페이지와 브라우저 사이에서 작동하는 프록시 스크립트
+     * 오프라인 모드, 푸시, 백그라운드 데이터 동기화 등의 기능 지원
+     * {@link https://vite-pwa-org.netlify.app/guide/}
+     * */
+    VitePWA({ registerType: 'autoUpdate' }),
     tsconfigPaths(),
     svgr({
       svgrOptions: {
