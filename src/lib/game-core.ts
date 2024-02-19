57ee8e4dc658af54d153ee3aa7fb0616ee90b174
ColorFilter <johan@romantech.net>
2024-02-20 00:23:25 +0900

    game-core 파일 코드 정리

--- src/lib/game-core.ts
+++ src/lib/game-core.ts
@@ -1,26 +1,24 @@
 import { BasePlayer, BoardSize, defaultSquare } from '@/lib/constants';
-import { getRandomElement } from '@/lib/helpers';
+import { selectRandomElement } from '@/lib/helpers';
 
 import { Identifier, TBoard, TMark, TSequence, TSquareColor, Winner } from './types';
 
-export const getCoordinatesFromIdx = (i: number, size: number) => {
-  const row = Math.floor(i / size);
-  const col = i % size; // 항상 0 ~ (size - 1) 값 반환
-  return { row, col };
-};
+/* ==========================================================================================
+ * ============================= Winning Condition Verification =============================
+ * ========================================================================================== */
 
 /**
  * 보드의 모든 위치를 검사하면 보드가 커질수록 비효율적이므로,
  * 마지막 놓았던 위치부터 가로, 세로, 대각선 방향으로 winCondition 만큼 검사
  * */
-export const checkWin = (
+export const checkWinIndexes = (
   board: TBoard,
   size: number,
   winCondition: number,
   linearIndex: number,
   player: BasePlayer,
 ) => {
-  const { row: lastRow, col: lastCol } = getCoordinatesFromIdx(linearIndex, size);
+  const { row: lastRow, col: lastCol } = getCoordinates(linearIndex, size);
 
   const directions = [
     { deltaRow: 0, deltaCol: 1 }, // 가로
@@ -30,7 +28,7 @@ export const checkWin = (
   ];
 
   for (const { deltaRow, deltaCol } of directions) {
-    const winningIndices = checkDirection(
+    const winningIndexes = checkDirection(
       board,
       size,
       lastRow,
@@ -42,7 +40,7 @@ export const checkWin = (
     );
 
     // 승리한 위치의 인덱스 배열 반환
-    if (winningIndices) return winningIndices.map(({ row, col }) => getLinearIndex(row, col, size));
+    if (winningIndexes) return winningIndexes.map(({ row, col }) => getLinearIndex(row, col, size));
   }
 
   return null;
@@ -51,14 +49,14 @@ export const checkWin = (
 const checkDirection = (
   board: TBoard,
   size: number,
-  row: number,
-  col: number,
+  lastRow: number,
+  lastCol: number,
   winCondition: number,
   player: BasePlayer,
   deltaRow: number,
   deltaCol: number,
 ) => {
-  const winningIndices = [{ row, col }]; // 승리한 위치의 인덱스 저장
+  const winningIndexes = [{ row: lastRow, col: lastCol }]; // 승리한 위치의 인덱스 저장
 
   // 한 방향으로 검사
   for (let i = 1; i < winCondition; i++) {
@@ -69,11 +67,11 @@ const checkDirection = (
      * 우측 하단 대각선 : row -> 3, 4 | col -> 3, 4
      * 좌측 하단 대각선 : row -> 3, 4 | col -> 1, 0
      * */
-    const currentRow = row + i * deltaRow;
-    const currentCol = col + i * deltaCol;
-    if (getCell(board, size, currentRow, currentCol) !== player) break;
+    const currentRow = lastRow + i * deltaRow;
+    const currentCol = lastCol + i * deltaCol;
+    if (getSquareIdentifier(board, size, currentRow, currentCol) !== player) break;
 
-    winningIndices.push({ row: currentRow, col: currentCol });
+    winningIndexes.push({ row: currentRow, col: currentCol });
   }
 
   // 반대 방향으로 검사
@@ -85,55 +83,89 @@ const checkDirection = (
      * 좌측 상단 대각선 : row -> 1, 0 | col -> 1, 0
      * 우측 상단 대각선 : row -> 1, 0 | col -> 3, 4
      * */
-    const currentRow = row - i * deltaRow;
-    const currentCol = col - i * deltaCol;
-    if (getCell(board, size, currentRow, currentCol) !== player) break;
+    const currentRow = lastRow - i * deltaRow;
+    const currentCol = lastCol - i * deltaCol;
+    if (getSquareIdentifier(board, size, currentRow, currentCol) !== player) break;
 
-    winningIndices.push({ row: currentRow, col: currentCol });
+    winningIndexes.push({ row: currentRow, col: currentCol });
   }
 
-  return winningIndices.length >= winCondition ? winningIndices : null;
+  return winningIndexes.length >= winCondition ? winningIndexes : null;
 };
+
+/* ==========================================================================================
+ * ================================ Best Move Calculation ===================================
+ * ========================================================================================== */
+
+export const findBestMoveIdx = (
+  board: TBoard,
+  size: number,
+  winCondition: number,
+  player: BasePlayer,
+) => {
+  const opponent = getOpponent(player);
+
+  // 승리할 수 있는 위치 탐색
+  const bestMove = getFirstBestMoveIdx(board, size, winCondition, player);
+  if (bestMove !== null) return bestMove;
+
+  // 방어 해야하는 위치 탐색
+  const defenseMove = getFirstBestMoveIdx(board, size, winCondition, opponent);
+  if (defenseMove !== null) return defenseMove;
+
+  // 중앙, 모서리, 빈칸 중 랜덤하게 반환. 모든 칸이 다 찼으면 null 반환
+  return chooseStrategicPosition(board, size);
+};
+
+const getAvailableMoves = (board: TBoard) => {
+  return board.reduce((moves, cell, i) => {
+    if (cell.identifier === null) moves.add(i);
+    return moves;
+  }, new Set<number>());
+};
+
+const getFirstBestMoveIdx = (
+  board: TBoard,
+  size: number,
+  winCondition: number,
+  player: BasePlayer,
+) => {
+  const idx = board.findIndex((cell, i) => {
+    if (cell.identifier === null) {
+      const winningIndexes = checkWinIndexes(board, size, winCondition, i, player);
+      return winningIndexes !== null;
+    }
+    return false;
+  });
+
+  return idx !== -1 ? idx : null;
+};
+
+const chooseStrategicPosition = (board: TBoard, size: number) => {
+  const availableMoves = getAvailableMoves(board);
+  if (availableMoves.size === 0) return null;
+
+  // 3x3 보드는 중앙, 모서리를 먼저 선점하는게 게임 승리에 유리함
+  if (size === 3) {
+    const centerIdx = getCenterIndexes(size);
+    const cornerIndexes = getCornerIndexes(size);
+    const filtered = [...centerIdx, ...cornerIndexes].filter((idx) => availableMoves.has(idx));
+    if (filtered.length > 0) return selectRandomElement(filtered);
+  }
+
+  return selectRandomElement([...availableMoves]);
+};
+
+/* ==========================================================================================
+ * =================================== Index Utilities ======================================
+ * ========================================================================================== */
 
 const getLinearIndex = (row: number, col: number, size: number) => row * size + col;
 
-/**
- * 2차원 보드를 기준으로한 row, col 값을 받아서 1차원 배열로 표현했을 때의 인덱스 반환
- * row * size = row 위치. e.g. 2(row) * 5(size) = 10 (3번째 행 첫번째 열)
- * 계산한 row 위치에서 col 더하면 해당 위치의 인덱스
- * */
-const getCell = (board: TBoard, size: number, row: number, col: number) => {
-  const linearIndex = getLinearIndex(row, col, size);
-  if (row >= 0 && row < size && col >= 0 && col < size) return board[linearIndex].identifier;
-  return null;
-};
-
-export const getInitialBoard = (size: number): TBoard => {
-  return Array(size * size).fill(defaultSquare);
-};
-
-export const createSquare = (
-  identifier: Identifier,
-  mark: TMark,
-  sequence: TSequence,
-  color: TSquareColor,
-) => ({
-  identifier,
-  mark,
-  sequence,
-  color,
-});
-
-export type TGameHistory = ReturnType<typeof createHistory>;
-export const createHistory = (board: TBoard, winner: Winner, size: BoardSize) => ({
-  board,
-  winner,
-  size,
-  createdAt: new Date().toISOString(),
-});
-
-export const getOpponent = (player: BasePlayer) => {
-  return player === BasePlayer.X ? BasePlayer.O : BasePlayer.X;
+export const getCoordinates = (i: number, size: number) => {
+  const row = Math.floor(i / size);
+  const col = i % size; // 항상 0 ~ (size - 1) 값 반환
+  return { row, col };
 };
 
 const getCenterIndexes = (size: number) => {
@@ -159,61 +191,45 @@ const getCornerIndexes = (size: number) => {
   return [leftTop, rightTop, leftBottom, rightBottom];
 };
 
-const getAvailableMoves = (board: TBoard) => {
-  return board.reduce((moves, cell, i) => {
-    if (cell.identifier === null) moves.add(i);
-    return moves;
-  }, new Set<number>());
+/* ==========================================================================================
+ * ================================== Board Utilities =======================================
+ * ========================================================================================== */
+
+/**
+ * 2차원 보드를 기준으로한 row, col 값을 받아서 1차원 배열로 표현했을 때의 인덱스 반환
+ * row * size = row 위치. e.g. 2(row) * 5(size) = 10 (3번째 행 첫번째 열)
+ * 계산한 row 위치에서 col 더하면 해당 위치의 인덱스
+ * */
+const getSquareIdentifier = (board: TBoard, size: number, row: number, col: number) => {
+  const linearIndex = getLinearIndex(row, col, size);
+  if (row >= 0 && row < size && col >= 0 && col < size) return board[linearIndex].identifier;
+  return null;
 };
 
-const getFirstBestMovePosition = (
-  board: TBoard,
-  size: number,
-  winCondition: number,
-  player: BasePlayer,
-) => {
-  const idx = board.findIndex((cell, i) => {
-    if (cell.identifier === null) {
-      const winnerIndices = checkWin(board, size, winCondition, i, player);
-      return winnerIndices !== null;
-    }
-    return false;
-  });
-
-  return idx !== -1 ? idx : null;
+export const getInitialBoard = (size: number): TBoard => {
+  return Array(size * size).fill(defaultSquare);
 };
 
-const chooseStrategicPosition = (board: TBoard, size: number) => {
-  const availableMoves = getAvailableMoves(board);
-  if (availableMoves.size === 0) return null;
-
-  // 3x3 보드는 중앙, 모서리를 먼저 선점하는게 게임 승리에 유리함
-  if (size === 3) {
-    const centerIdx = getCenterIndexes(size);
-    const cornerIndexes = getCornerIndexes(size);
-    const filtered = [...centerIdx, ...cornerIndexes].filter((idx) => availableMoves.has(idx));
-    if (filtered.length > 0) return getRandomElement(filtered);
-  }
-
-  return getRandomElement([...availableMoves]);
+export const getOpponent = (player: BasePlayer) => {
+  return player === BasePlayer.X ? BasePlayer.O : BasePlayer.X;
 };
 
-export const findBestMove = (
-  board: TBoard,
-  size: number,
-  winCondition: number,
-  player: BasePlayer,
-) => {
-  const opponent = getOpponent(player);
+export const createSquare = (
+  identifier: Identifier,
+  mark: TMark,
+  sequence: TSequence,
+  color: TSquareColor,
+) => ({
+  identifier,
+  mark,
+  sequence,
+  color,
+});
 
-  // 승리할 수 있는 위치 탐색
-  const bestMove = getFirstBestMovePosition(board, size, winCondition, player);
-  if (bestMove !== null) return bestMove;
-
-  // 방어 해야하는 위치 탐색
-  const defenseMove = getFirstBestMovePosition(board, size, winCondition, opponent);
-  if (defenseMove !== null) return defenseMove;
-
-  // 중앙, 모서리, 빈칸 중 랜덤하게 반환. 모든 칸이 다 찼으면 null 반환
-  return chooseStrategicPosition(board, size);
-};
+export type TGameHistory = ReturnType<typeof createHistory>;
+export const createHistory = (board: TBoard, winner: Winner, size: BoardSize) => ({
+  board,
+  winner,
+  size,
+  createdAt: new Date().toISOString(),
+});
